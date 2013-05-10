e4ce64894fa89c181a72ae93dff36353a50e42d7
dwin999 <duncan@winfrey.co.uk>
2013-05-10 22:41:53 +0100

    added bing

--- dns_dork.py
+++ dns_dork.py
@@ -50,17 +50,59 @@ def get_args():
     
     parser = argparse.ArgumentParser('dns_dork.py', formatter_class=lambda prog:argparse.HelpFormatter(prog,max_help_position=40))
     parser.add_argument('-t', '--target', help='Target domain', dest='target', required=True)
+    parser.add_argument('-g', '--google', action="store_true", default=True, help='Use google search', dest='g', required=False)
+    parser.add_argument('-b', '--bing', action="store_true", default=False, help='Use bing API', dest='b', required=False)
     parser.add_argument('-v', '--verbose', action="store_true", default=False, help='Verbose mode', dest='verbose', required=False)
     args = parser.parse_args()
-
-def find_subdomains(searchstrings):
+    
+def bing_subdomains(searchstrings):
+    global args
+    user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
+    key= ''
+    creds = (':%s' % key).encode('base64')[:-1]
+    auth = 'Basic %s' % creds
+    url = 'https://api.datamarket.azure.com/Data.ashx/Bing/Search/Web?Query=%27'
+    data = "site%3A"+args.target+'%20-www.'+args.target+searchstrings
+    url = url + data + "%27&$top=50&$format=json"
+    if(len(str(url)) > 2048):
+        end(subdomainlist)
+        exit()
+    request = urllib2.Request(url)
+    request.add_header('Authorization', auth)
+    request.add_header('User-Agent', user_agent)
+    response = urllib2.urlopen(request)
+    results = json.load(response)
+    startlen = len(subdomainlist)
+    for reply in results['d']['results']:
+        if reply['Url'] != None:
+            string = reply['Url']
+            print '\n=[Link]= '
+            print string.encode('utf-8')
+            string = string.replace("http://", "")
+            string = string.replace("https://", "")
+            subdomain = string.split("/")
+            #print subdomain[0] + str(len(subdomainlist))
+            subdomainlist.append(subdomain[0].encode('utf-8')) # subdomain[0] is unicode, cast it to str
+    if startlen == len(subdomainlist):
+        print "no more domains"
+        print end(subdomainlist)
+        sys.exit()
+def bing_update_string(xlist):
+    searchstrings = ""
+    for item in xlist:     
+        searchstrings = searchstrings + '%20-domain%3A'+item
+    #error in that the first string cannot have %20 
+    searchstrings = searchstrings[3:]
+    print searchstrings
+    return searchstrings
+        
+def google_subdomains(searchstrings):
     global args
     url = 'http://ajax.googleapis.com/ajax/services/search/web?'
     params = { 'q': "site:"+args.target+' -site:www.'+args.target+searchstrings}
     data = urllib.urlencode(params)
     url = url + data + '&v=1.0'
     out.verbose(url.encode('utf-8'))
-
     request = urllib2.Request( url,None, {'Referer': 'http://www.duckduckgo.com' })
     response = urllib2.urlopen(request)
     results = json.loads(response.read().decode("utf-8"))
@@ -86,6 +128,12 @@ def update_string(xlist):
         searchstrings = searchstrings + ' -site:'+item
     return searchstrings
 
+def end(subdomainlist):
+    subdomainlist = list(set(subdomainlist)) # removing any duplicate entires
+    subdomainlist.sort()
+    for item in subdomainlist:
+        print item
+    print "Total subs: " + str(len(subdomainlist))
 
 if __name__ == "__main__":
     global args
@@ -94,10 +142,12 @@ if __name__ == "__main__":
     subdomainlist = []
             
     for x in range(20):
-        subdomainlist = list(set(subdomainlist)) # removing any duplicate entires
-        searchstrings = update_string(subdomainlist)  # create search string from subdomain list
-        find_subdomains(searchstrings)  # find those strings 
-
-    subdomainlist = list(set(subdomainlist)) # removing any duplicate entires
-    subdomainlist.sort()
-    print(subdomainlist)
+        subdomainlist = list(set(subdomainlist)) # removing any duplicate entires      
+        if(args.g):
+            searchstrings = update_string(subdomainlist)  # create search string from subdomain list
+            google_subdomains(searchstrings)  # find those strings 
+        if(args.b):
+            searchstrings = bing_update_string(subdomainlist)  # create search string from subdomain list
+            bing_subdomains(searchstrings)  # find those strings
+            
+    end(subdomainlist)
